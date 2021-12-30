ba536b4f51558cd9d7abf0992904f94fef48e2d4
Valentin Dumay-Pothuon <valentin.dumay.pothuon@gmail.com>
2021-12-30 15:17:58 +0100

    30122021-1 : Dissociated password schema from middleware

--- backend/middleware/checkPassword.js
+++ backend/middleware/checkPassword.js
@@ -1,19 +1,9 @@
-const passwordValidator = require('password-validator');
+const passwordSchema = require('../models/Password')
 
 module.exports = (req, res, next) => {
     const isValidPassword = (password) => {
-        // Create a schema
-        let schema = new passwordValidator();
-        // Add properties to it
-        schema
-            .is().min(8)                  // Minimum length 8
-            .is().max(100)                // Maximum length 100
-            .has().uppercase()            // Must have uppercase letters
-            .has().lowercase()            // Must have lowercase letters
-            .has().digits(1)              // Must have at least 1 digit
-            .has().not().spaces()         // Should not have spaces
-
-        if (schema.validate(password)) {
+    
+        if (passwordSchema.validate(password)) {
             next();
         } else {
             res.status(400).json({
